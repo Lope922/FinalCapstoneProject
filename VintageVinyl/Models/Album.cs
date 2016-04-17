0571584b0b759e276f291f945d7636a7e0435439
CaLs_Rig <Lope922@gmail.com>
2016-04-16 22:44:36 -0500

    initial commit

---
+++ VintageVinyl/Models/Album.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Web;
+
+namespace VintageVinyl.Models
+{
+    public class Album
+    {
+        public int ID { get; set; }
+        public string AlbumName { get; set; }
+        public DateTime DateIn { get; set; }
+
+        // question mark allows nulls in this field ,because albums don't have a sell date yet 
+        public DateTime? DateOut { get; set; }
+
+        //TODO modify this one i have the basics working. 
+         // would also like to add types , conidition , and initial price, 
+
+        // want the same functionality as in Cosignors through thier relationships
+        public virtual ICollection<Cosignor> Cosignors { get; set; } 
+
+        // from here i am moving onto the DAL Data Access Layer
+    }
+}
---
+++ VintageVinyl/Models/Cosignor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VintageVinyl.Models
+{
+    public class Cosignor
+    {
+        //pk
+        public int ID { get; set; }
+
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        public decimal PhoneNumber { get; set; }
+        // the virtual allows the properties of this class to be navigatable. This allows them to take advantage of certain entity frameworks functionality such as lazy loading 
+        public virtual ICollection<Album> Albums { get; set; } 
+
+    }
+}
